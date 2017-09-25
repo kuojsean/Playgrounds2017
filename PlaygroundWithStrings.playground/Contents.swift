4c5f4971472df227afcdf16d95f0ddda2f6058d1
kuojsean <kuo.j.sean@gmail.com>
2017-09-25 09:08:56 -0600

    Created a new Playground and assigned string to constants and variables following the language guide.

---
+++ PlaygroundWithStrings.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var sampleString :String
+
+let secondString = "Random values"
+
+print(secondString)
+
+let multilineQuotes = "At the end of the day, you are solely responsible for your success and your failure. And the sooner you realize that, you accept that, and integrate that into your work ethic, you will start being successful. As long as you blame others for the reason you aren't where you want to be, you will always be a failure."
+
+print(multilineQuotes)
+
+let emojisAreAwesome = "\"Imagination is more important than knowledge\" - Einstein"
+// "Imagination is more important than knowledge" - Einstein
+print(emojisAreAwesome)
+
+let emojiSparklingHeart = "\u{1F496}" //💖, Unicode scalar U+1F496
+print(emojiSparklingHeart)
+
+sampleString = ""
+var fillerString = String()
+
+if sampleString.isEmpty {
+    print("Get Out!")
+}
+
+
+
+
+
