e43b8c29e8e72a5553d481d3432e219e6fe6ea9d
kuojsean <kuo.j.sean@gmail.com>
2017-09-25 08:36:55 -0600

    Experimented with variables

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -1,5 +1,30 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-var str = "Hello, playground"
+//Explicit Type declaration with no value
+var ramen :String
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge + 54322456
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Sean"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
+var Dub :Double
+Dub = 6.9
+print(Dub)
+Dub = Dub * 123561646.2
+print(Dub)
