d2d41645dc4115ce166e3eef25439e42ec39b905
kuojsean <kuo.j.sean@gmail.com>
2017-10-04 08:24:42 -0600

    Created my second UI Button.

--- GUI Playground.playground/Contents.swift
+++ GUI Playground.playground/Contents.swift
@@ -8,3 +8,9 @@ firstButton.backgroundColor = .orange
 firstButton.setTitle("Button", for: .normal)
 var temp = firstButton.titleLabel
 firstButton.setTitle("Look a button", for: .normal)
+
+var secondButton = UIButton(frame: CGRect(x: 50, y: 50, width: 250, height: 50))
+secondButton.backgroundColor = .magenta
+secondButton.setTitle("My Second Button", for: .normal)
+var tempTwo = secondButton.titleLabel
+secondButton.setTitle("Aw Geez Rick", for: .normal)
